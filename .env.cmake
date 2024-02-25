595791adb9ee412420e15d86e9999d05c1ef2254
Gleb Ezhov <glebid76@gmail.com>
2024-02-25 13:24:04 +0400

    Make .env.cmake as a template for portability.

--- .env.cmake
+++ .env.cmake
@@ -1,10 +1,17 @@
-﻿set(GLFW_PATH D:/Development/VisualStudio/dir_Libraries/glfw-3.3.5.bin.WIN64)
-set(GLM_PATH D:/Development/VisualStudio/dir_Libraries/glm-master)
-set(VULKAN_SDK_PATH  D:/Soft/VulkanSDK/1.2.189.0)
+﻿## REQUIRED
+# Set GLFW_PATH like this:
+# set(GLFW_PATH X:/dev/Libraries/glfw-3.3.5.bin.WIN64)
 
-# Set MINGW_PATH if using mingwBuild.bat and not VisualStudio20XX
+# Set GLM_PATH like this:
+# set(GLM_PATH X:/dev/Libraries/glm-master)
+
+# Set VULKAN_SDK_PATH like this:
+# set(VULKAN_SDK_PATH X:/dev/Libraries/1.2.x.x)
+
+# Set MINGW_PATH if using mingwBuild.bat and not VisualStudio20XX like this:
 # set(MINGW_PATH "C:/Program Files/mingw-w64/x86_64-8.1.0-posix-seh-rt_v6-rev0/mingw64")
 
+## OPTIONAL
 # Optional set TINYOBJ_PATH to target specific version, otherwise defaults to external/tinyobjloader
 # set(TINYOBJ_PATH X:/dev/Libraries/tinyobjloader)
 
