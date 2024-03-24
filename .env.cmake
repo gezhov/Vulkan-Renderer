99c8e6de2ea53bb5e3cf0992b3358f1392353b3d
Gleb Ezhov <glebid76@gmail.com>
2024-03-24 23:38:38 +0400

    .env.cmake change.

--- .env.cmake
+++ .env.cmake
@@ -1,25 +1,16 @@
-﻿## REQUIRED
-# Set GLFW_PATH like this:
+﻿## OPTIONAL. Set variables to target specific version of library.
 # set(GLFW_PATH X:/dev/Libraries/glfw-3.3.5.bin.WIN64)
 
-# Set GLM_PATH like this:
 # set(GLM_PATH X:/dev/Libraries/glm-master)
 
-# Set VULKAN_SDK_PATH like this:
 # set(VULKAN_SDK_PATH X:/dev/Libraries/1.2.x.x)
 
-# Set MINGW_PATH if using mingwBuild.bat and not VisualStudio20XX like this:
 # set(MINGW_PATH "C:/Program Files/mingw-w64/x86_64-8.1.0-posix-seh-rt_v6-rev0/mingw64")
 
-## OPTIONAL
-# Optional set TINYOBJ_PATH to target specific version, otherwise defaults to external/tinyobjloader
 # set(TINYOBJ_PATH X:/dev/Libraries/tinyobjloader)
 
-# Optional set STB_IMAGE_PATH to target specific version, otherwise defaults to external/stb_image
 # set(STB_IMAGE_PATH X:/dev/Libraries/stb_image)
 
-# Optional set IMGUI_PATH to target specific version, otherwise defaults to external/imgui
 # set(IMGUI_PATH X:/dev/Libraries/imgui)
 
-# Optional set IMGUIZMO_PATH to target specific version, otherwise defaults to external/imguizmo
 # set(IMGUIZMO_PATH X:/dev/Libraries/imguizmo)
